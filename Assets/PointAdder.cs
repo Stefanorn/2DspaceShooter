cc40405969826beafb0e902173079f3543399160
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2014-12-22 01:03:58 +0100

    Henti inn scoreTracker

---
+++ Assets/PointAdder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointAdder : MonoBehaviour {
+
+	public int points = 10; 
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D () 
+	{
+		ScoreTracker.AddPoint (points);
+	}
+}
---
+++ Assets/ScoreTracker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ScoreTracker : MonoBehaviour {
+
+	static int scoreTracker = 0;
+	static int highScore = 0;
+
+	Text textBox;
+
+	// Use this for initialization
+	void Start () 
+	{
+		scoreTracker = 0;
+		textBox = GetComponent <Text> ();
+		highScore = PlayerPrefs.GetInt ("highScore", 0);
+	
+	}
+	static public void AddPoint(int points)
+	{
+		scoreTracker = scoreTracker + points;
+		if (scoreTracker > highScore) {
+			highScore = scoreTracker;
+		}
+	}
+	void OnDestroy()
+	{
+		PlayerPrefs.SetInt ("highScore", highScore);
+	}
+	// Update is called once per frame
+	void Update () {
+		textBox.text = "Score : " + scoreTracker;
+	}
+}
