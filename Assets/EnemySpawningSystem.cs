99d25e21dc8e43421a41c437a17a788999e32288
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2014-12-28 23:20:13 +0100

    Bættti við fleirri patterns inní enemy spawnerinn

--- Assets/EnemySpawningSystem.cs
+++ Assets/EnemySpawningSystem.cs
@@ -4,51 +4,48 @@ using System.Collections;
 public class EnemySpawningSystem : MonoBehaviour {
 
 	public GameObject[] enemys;
-	public string[] corutines;
+	string[] corutines = {"Box", "SnakeSpawnX"};
 	public float spawnRate = 0.1f;
+	int enemyPicker = 0;
 
 	float xValue;
 	float yValue;
-	float offset = 1f;
+	public float offset = 2f;
 	Vector2 spawnLocation;
 
 	void Start(){
-		StartCoroutine (corutines[Random.Range(0,corutines.Length-1)]);
-	}
-	IEnumerator Box (){
-
 		yValue = Camera.main.orthographicSize;
 		xValue = Camera.main.orthographicSize * ((float)Screen.width / (float)Screen.height);
 		float xValueMax = Camera.main.orthographicSize * ((float)Screen.width / (float)Screen.height);
 		spawnLocation.y = yValue;
 
+		//StartCoroutine (corutines[Random.Range(0,corutines.Length)]);
+		StartCoroutine (SnakeSpawnX ());
+	}
+	IEnumerator Box (){
+		float xValueMax = Camera.main.orthographicSize * ((float)Screen.width / (float)Screen.height);
 		StartCoroutine (XLine(1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate)/offset);
+		yield return new WaitForSeconds (spawnRate*((xValueMax /offset)*2) + offset);
 		StartCoroutine (YLine(1) );
-		yield return new WaitForSeconds (spawnRate*(yValue /spawnRate));
+		yield return new WaitForSeconds (spawnRate*(Camera.main.orthographicSize * 2 /offset) + offset);
 		StartCoroutine (XLine(-1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate)/offset);
+		yield return new WaitForSeconds (spawnRate*((xValueMax /offset)*2) + offset);
 		StartCoroutine (YLine(-1) );
-		yield return new WaitForSeconds (spawnRate*(-yValue /spawnRate)/offset);
+		yield return new WaitForSeconds (spawnRate*(Camera.main.orthographicSize * 2 / offset) + offset);
 
 		Start ();
 	}
-	IEnumerator SickSack(){
-
-		yValue = Camera.main.orthographicSize;
-		xValue = Camera.main.orthographicSize * ((float)Screen.width / (float)Screen.height);
+	IEnumerator SnakeSpawnX(){
+		xValue = 0f;
 		float xValueMax = Camera.main.orthographicSize * ((float)Screen.width / (float)Screen.height);
-		spawnLocation.y = yValue;
-
-		StartCoroutine (XLine(1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate));
-		StartCoroutine (XLine(-1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate));
-		StartCoroutine (XLine(1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate));
-		StartCoroutine (XLine(-1) );
-		yield return new WaitForSeconds (spawnRate*(xValueMax /spawnRate));
-
+		enemyPicker = Random.Range (0, enemys.Length);
+		for(float f = 0; f < 50 ;f++ ){
+			spawnLocation.x = xValue;
+			Instantiate(enemys[enemyPicker], spawnLocation, Quaternion.Euler(0,0,180));
+			Debug.Log(spawnLocation);
+			xValue += Random.Range(-offset, offset);
+			yield return new WaitForSeconds(spawnRate);
+		}
 		Start ();
 	}
 	IEnumerator XLine( float posOrNeg ){
@@ -56,8 +53,7 @@ public class EnemySpawningSystem : MonoBehaviour {
 		xValue = -xValueMax * posOrNeg;
 		for(float f = 0; f < (xValueMax/offset)*2 ;f++ ){
 			spawnLocation.x = xValue;
-			Instantiate(enemys[0], spawnLocation, Quaternion.Euler(0,0,90 * (1 + posOrNeg) ));
-			Debug.Log (spawnLocation);
+			Instantiate(enemys[4], spawnLocation, Quaternion.Euler(0,0,90 * (1 + posOrNeg) ));
 			xValue += offset *posOrNeg;
 			yield return new WaitForSeconds(spawnRate);
 		}
@@ -67,8 +63,7 @@ public class EnemySpawningSystem : MonoBehaviour {
 
 		for(float f = 0; f < (yValueMax/offset) ;f++ ){
 			spawnLocation.y = yValue;
-			Instantiate(enemys[0], spawnLocation, Quaternion.Euler(0,0,90* posOrNeg));
-			Debug.Log (spawnLocation);
+			Instantiate(enemys[4], spawnLocation, Quaternion.Euler(0,0,90* posOrNeg));
 			yValue -= offset * posOrNeg;
 			yield return new WaitForSeconds(spawnRate);
 		}
